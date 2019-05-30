be660c0935db61988265edb6c5193281937f268c
luszowie <luszowie@wszib.edu.pl>
2019-05-30 21:31:16 +0200

    2

---
+++ src/test/java/pl/edu/wszib/projekt/dao/FilmDaoTest.java
@@ -0,0 +1,17 @@
+package pl.edu.wszib.projekt.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FilmDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByTytul() {
+    }
+}
